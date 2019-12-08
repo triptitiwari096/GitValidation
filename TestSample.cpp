12448df5f14694d1afe23e00895c1a597b523feb
triptitiwari096 <58257638+triptitiwari096@users.noreply.github.com>
2019-12-08 20:35:52 +0530

    Hello World

--- TestSample.cpp
+++ TestSample.cpp
@@ -2,13 +2,7 @@
 using namespace std;
 int main()
 {
-    int firstNumber, secondNumber, sumOfTwoNumbers;
     
-    cout << "Enter two integers: ";
-    cin >> firstNumber >> secondNumber;
-    // sum of two numbers in stored in variable sumOfTwoNumbers
-    sumOfTwoNumbers = firstNumber + secondNumber;
-    // Prints sum 
-    cout << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
-    return 0;
+    cout << "Hello World";
+    
 }
